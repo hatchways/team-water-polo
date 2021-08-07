6c6a217caba0c1e0c347042058934061231905cb
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-08-07 07:08:12 -0400

    create route for updating and moving cards

--- server/controllers/card.js
+++ server/controllers/card.js
@@ -21,3 +21,30 @@ exports.createCard = asyncHandler(async (req, res, next)=> {
   column.save()
   res.status(200).json(newCard)
 })
+
+// Update Card title or tag
+exports.updateCard = asyncHandler(async (req, res)=> {
+  const {name, tag} = req.body
+  const card = await Card.findById(req.params.id)
+  card.name = name
+  card.tag  = tag
+  card.save()
+  res.status(200).json(card)
+})
+
+// Move Card between columns
+exports.moveCard = asyncHandler(async (req, res)=> {
+  const {currentColumnId, newColumnId} = req.body
+  const card = await Card.findById(req.params.id)
+  card.columnId = newColumnId
+  card.save()
+
+  // updating columns
+  const currentColumn = await Column.findById(currentColumnId)
+  currentColumn.cards.pull(req.params.id)
+  currentColumn.save()
+  const newColumn = await Column.findById(newColumnId)
+  newColumn.cards.push(req.params.id)
+  newColumn.save()
+  res.status(200).json(card)
+})
--- server/routes/card.js
+++ server/routes/card.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/auth');
 const {
-  createCard
+  createCard,
+  updateCard,
+  moveCard
 } = require('../controllers/card');
 
 router.route('/').post(protect, createCard);
+router.route('/:id/update').patch(protect, updateCard);
+router.route('/:id/move').patch(protect, moveCard);
 
 module.exports = router;
